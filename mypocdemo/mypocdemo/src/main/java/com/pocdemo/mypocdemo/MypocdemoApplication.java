530dec9ec2d59bc70a938ac751f7836a64adc225
deepak sharma <deepak.sharma@globant.com>
2022-03-09 12:02:41 +0530

    second commit

---
+++ mypocdemo/mypocdemo/src/main/java/com/pocdemo/mypocdemo/MypocdemoApplication.java
@@ -0,0 +1,13 @@
+package com.pocdemo.mypocdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypocdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypocdemoApplication.class, args);
+	}
+
+}
---
+++ mypocdemo/mypocdemo/src/test/java/com/pocdemo/mypocdemo/MypocdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocdemo.mypocdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypocdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
